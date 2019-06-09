470f427460ff4de590e15bcf0434c393481e9678
Cahil Foley <cahilfoley2@gmail.com>
2019-06-09 23:09:20 +0800

    docs(demo): add interactive demo

---
+++ demo/src/components/Settings/theme.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react'
+import { createMuiTheme, MuiThemeProvider } from '@material-ui/core/styles'
+
+export const theme = createMuiTheme({
+  palette: {
+    type: 'dark',
+    primary: {
+      main: 'rgba(33, 150, 243, 0.87)',
+    },
+  },
+})
+
+export const ThemeProvider: React.FC = ({ children }) => {
+  return <MuiThemeProvider theme={theme}>{children}</MuiThemeProvider>
+}
