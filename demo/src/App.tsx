f6c64ed9baf91e6c3a1c5857e039b6cf0334865b
Cahil Foley <cahilfoley2@gmail.com>
2019-06-10 19:00:26 +0800

    docs(demo): move background image to public folder

--- demo/src/App.tsx
+++ demo/src/App.tsx
@@ -1,38 +1,31 @@
-import React, { useContext } from 'react'
-import Snowfall from 'react-snowfall'
-import GithubLink from './components/GithubLink/GithubLink'
-import Settings from './components/Settings'
-import { SettingsContext, StateProvider } from './context/settings'
-import './App.css'
+import React, { useContext } from "react";
+import Snowfall from "react-snowfall";
+import GithubLink from "./components/GithubLink/GithubLink";
+import Settings from "./components/Settings";
+import { SettingsContext, StateProvider } from "./context/settings";
+import "./App.css";
 
-const githubURL = process.env.REACT_APP_GITHUB_URL as string
-const packageName = process.env.REACT_APP_PACKAGE_NAME as string
+const githubURL = process.env.REACT_APP_GITHUB_URL as string;
+const packageName = process.env.REACT_APP_PACKAGE_NAME as string;
 
-const Demo = () => {
-  const settings = useContext(SettingsContext)
-
-  return <Snowfall color={settings.color} snowflakeCount={settings.snowflakeCount} />
-}
-
-const Title = () => {
-  const settings = useContext(SettingsContext)
+const App = () => {
+  const settings = useContext(SettingsContext);
 
   return (
-    <a href={githubURL} style={{ color: settings.color }}>
-      <h1>{packageName}</h1>
-    </a>
-  )
-}
-
-const App = () => (
-  <StateProvider>
-    <div className="app">
+    <StateProvider>
+      <div className="app">
+        <Snowfall
+          color={settings.color}
+          snowflakeCount={settings.snowflakeCount}
+        />
+        <a href={githubURL} style={{ color: settings.color }}>
+          <h1>{packageName}</h1>
+        </a>
+        <Settings />
+      </div>
       <GithubLink url={githubURL} />
-      <Title />
-      <Demo />
-      <Settings />
-    </div>
-  </StateProvider>
-)
+    </StateProvider>
+  );
+};
 
-export default App
+export default App;
--- demo/src/components/Settings/Settings.tsx
+++ demo/src/components/Settings/Settings.tsx
@@ -1,42 +1,44 @@
-import React, { useContext } from 'react'
-import { CirclePicker } from 'react-color'
-import Box from '@material-ui/core/Box'
-import Paper from '@material-ui/core/Paper'
-import Typography from '@material-ui/core/Typography'
-import Slider from '@material-ui/lab/Slider'
-import { SettingsContext, SnowfallSettings } from 'context/settings'
-import { ThemeProvider } from './theme'
+import React, { useContext } from "react";
+import { CirclePicker } from "react-color";
+import Box from "@material-ui/core/Box";
+import Paper from "@material-ui/core/Paper";
+import Typography from "@material-ui/core/Typography";
+import Slider from "@material-ui/lab/Slider";
+import { SettingsContext, SnowfallSettings } from "../../context/settings";
+import { ThemeProvider } from "./theme";
 
-import './Settings.css'
+import "./Settings.css";
 
 const colors = [
-  '#dee4fd',
-  '#e91e63',
-  '#9c27b0',
-  '#673ab7',
-  '#3f51b5',
-  '#2196f3',
-  '#03a9f4',
-  '#00bcd4',
-  '#009688',
-  '#4caf50',
-  '#8bc34a',
-  '#cddc39',
-  '#ffeb3b',
-  '#ffc107',
-  '#ff9800',
-  '#ff5722',
-  '#795548',
-  '#607d8b',
-]
+  "#dee4fd",
+  "#e91e63",
+  "#9c27b0",
+  "#673ab7",
+  "#3f51b5",
+  "#2196f3",
+  "#03a9f4",
+  "#00bcd4",
+  "#009688",
+  "#4caf50",
+  "#8bc34a",
+  "#cddc39",
+  "#ffeb3b",
+  "#ffc107",
+  "#ff9800",
+  "#ff5722",
+  "#795548",
+  "#607d8b"
+];
 
 const Settings = () => {
-  const settings = useContext(SettingsContext) as SnowfallSettings
+  const settings = useContext(SettingsContext) as SnowfallSettings;
 
   return (
     <ThemeProvider>
       <Paper className="settings-container">
-        <Typography gutterBottom>Snowflake Count - {settings.snowflakeCount}</Typography>
+        <Typography gutterBottom>
+          Snowflake Count - {settings.snowflakeCount}
+        </Typography>
         <Slider
           value={settings.snowflakeCount}
           min={0}
@@ -55,7 +57,7 @@ const Settings = () => {
         </Box>
       </Paper>
     </ThemeProvider>
-  )
-}
+  );
+};
 
-export default Settings
+export default Settings;
