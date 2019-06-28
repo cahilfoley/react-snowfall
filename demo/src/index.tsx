e4d3018151d7265a812fbbd6197fdec1a455a6ca
Cahil Foley <cahilfoley2@gmail.com>
2019-06-28 21:20:53 +0800

    docs: fix up web demo web app manifest

--- demo/src/index.tsx
+++ demo/src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 import * as serviceWorker from './serviceWorker'
-import { StateProvider } from 'context/settings'
+import { StateProvider } from './context/settings'
 
 ReactDOM.render(
   <StateProvider>
