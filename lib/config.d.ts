c21cdf86b8a4be091463a9f9ae5f101c5abc03cd
Cahil Foley <cahilfoley2@gmail.com>
2021-11-29 10:29:31 +0800

    chore(build): update library files with latest changes

--- lib/config.d.ts
+++ lib/config.d.ts
@@ -1,4 +1,3 @@
 import { CSSProperties } from 'react';
 export declare const snowfallBaseStyle: CSSProperties;
 export declare const targetFrameTime: number;
-export declare const requestAnimationFrame: ((callback: FrameRequestCallback) => number) & typeof globalThis.requestAnimationFrame;
